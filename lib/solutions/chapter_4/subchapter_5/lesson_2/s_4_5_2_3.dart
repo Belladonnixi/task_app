749d344d67c2a7a2b7489ff1a4ee63aea23a2a28
Belladonnixi <jessica_ernst@outlook.de>
2025-01-10 14:30:16 +0100

    add solved task

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,37 @@ class S4523 extends StatelessWidget {
   const S4523({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String name = '';
+
+  void _updateName() {
+    setState(() {
+      name = 'Jessi';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 24,
+      children: [
+        Text(name),
+        ElevatedButton(
+          onPressed: _updateName,
+          child: const Text('Name anzeigen'),
+        ),
+      ],
+    );
   }
 }
